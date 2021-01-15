9a526c24ac269e3d8533634e4992e7cad296201e
Shuxin Dong <shuxind11@gmail.com>
2021-01-15 09:36:44 -0700

    Update NO2_merge_birth.R

--- codes/merge_descrip/mergeNO2/NO2_merge_birth.R
+++ codes/merge_descrip/mergeNO2/NO2_merge_birth.R
@@ -5,7 +5,7 @@
 # Input: mabirths_02NOV18.csv (from Anna)                                     #
 # Input: NO2 daily prediction on QNAP3                                        #
 # Output: #
-# Author: Shuxin Dong                                                         #
+# Author: Shuxin Dong (modified from Yaguang's codes)                         #
 # Date: 2021-01-06                                                            #
 ###############################################################################
 
@@ -13,86 +13,125 @@
 rm(list=ls())
 gc()
 
-require(dplyr)
-require(magrittr)
-require(lubridate)
-require(data.table)
+library(data.table)
 library(nabor)
-library(fst)
+# require(dplyr)
+# require(magrittr)
+# require(lubridate)
+# library(fst)
 
-dir_birth <- "/media/qnap3/Shuxin/"
+setwd("/media/gate/Shuxin")
+dir_birth <- "/media/gate/Shuxin/"
 dir_no2 <- "/media/qnap3/Exposure modeling/3 National scale/USA/5 NO2 v1.2000_16/7 Final predictions 1 km/Daily/"
-dir_output <- "/media/qnap3/Shuxin/"
+dir_output <- "/media/gate/Shuxin/"
+## read in birth data and convert formats
+birth <- fread(paste0(dir_birth, "mabirths_02NOV18.csv"))
+names(birth)
+dat <- birth[,.(uniqueid_yr, bdob, long, lat)]
+rm(birth)
+dat <- na.omit(dat)
+dat$id <- 1:nrow(dat)
+dat$bdob <- as.Date(dat$bdob, tryFormats = "%m/%d/%Y")
+## split raw birth data for memory saving
+dim(dat)[1]/2
+dat_1 <- dat[1:floor(dim(dat)[1]/2),]
+dat_2 <- dat[ceiling(dim(dat)[1]/2):dim(dat)[1], ]
 
-dat <- read.csv(paste0(dir_birth, "mabirths_02NOV18.csv"))
-# dat$id <- 1:nrow(dat)
-
-
-############################# 1. NO2 ########################################
-### identify grid id
-sitecode_no2 <- readRDS(paste0(dir_no2,'USGridSite.rds'))
-link_no2 <- nabor::knn(sitecode_no2[,c("Lon","Lat")],dat[,c("LONG","LAT")],k=1,radius=2*sqrt(2)/10*0.1)
-dat <- cbind.data.frame(dat,link_no2$nn.idx)
-names(dat)[4] <- "grid_id_no2"
-
+gc()
+##################### 1. extract NO2 data files ###############################
 ### extract exposures
-no2_files_200612 <- list.files(path=dir_no2,pattern = "^PredictionStep2_NO2_USGrid_200612(.*)rds$")
-no2_files_2007 <- list.files(path=dir_no2,pattern = "^PredictionStep2_NO2_USGrid_2007(.*)rds$")
-no2_files_2008 <- list.files(path=dir_no2,pattern = "^PredictionStep2_NO2_USGrid_2008(.*)rds$")
-no2_files_2009 <- list.files(path=dir_no2,pattern = "^PredictionStep2_NO2_USGrid_2009(.*)rds$")
-no2_files_2010 <- list.files(path=dir_no2,pattern = "^PredictionStep2_NO2_USGrid_2010(.*)rds$")
-no2_files_2011 <- list.files(path=dir_no2,pattern = "^PredictionStep2_NO2_USGrid_2011(.*)rds$")
-no2_files_201201 <- list.files(path=dir_no2,pattern = "^PredictionStep2_NO2_USGrid_201201(.*)rds$")
-no2_files <- c(no2_files_200612,no2_files_2007,no2_files_2008,no2_files_2009,no2_files_2010,no2_files_2011,no2_files_201201)
+no2_files_2000 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_200002(.*)rds$")
+no2_files_2001 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2001(.*)rds$")
+no2_files_2002 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2002(.*)rds$")
+no2_files_2003 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2003(.*)rds$")
+no2_files_2004 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2004(.*)rds$")
+no2_files_2005 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2005(.*)rds$")
+no2_files_2006 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2006(.*)rds$")
+no2_files_2007 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2007(.*)rds$")
+no2_files_2008 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2008(.*)rds$")
+no2_files_2009 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2009(.*)rds$")
+no2_files_2010 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2010(.*)rds$")
+no2_files_2011 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2011(.*)rds$")
+no2_files_2012 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2012(.*)rds$")
+no2_files_2013 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2013(.*)rds$")
+no2_files_2014 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2014(.*)rds$")
+no2_files_2015 <- list.files(path = dir_no2, pattern = "^PredictionStep2_NO2_USGrid_2015(.*)rds$")
+no2_files <- c(no2_files_2000, no2_files_2001, no2_files_2002, no2_files_2003, no2_files_2004, no2_files_2005,
+               no2_files_2006, no2_files_2007, no2_files_2008, no2_files_2009, no2_files_2010,
+               no2_files_2011, no2_files_2012, no2_files_2013, no2_files_2014, no2_files_2015)
 
-### merge
-no2 <- data.frame(matrix(vector(),0,5),stringsAsFactors = FALSE)
-names(no2) <- c("lon","lat",'id','no2','date')
-for (i in 1:length(no2_files)) {
+##################### 2. link NO2 to birth data ###############################
+sitecode_no2 <- readRDS(paste0(dir_no2, "USGridSite.rds"))
+
+########################## 1st part of birth data #############################
+## identify grid id
+link_no2 <- nabor::knn(sitecode_no2[, c("Lon","Lat")],
+                       dat_1[, c("long","lat")],
+                       k = 1, radius = 2*sqrt(2)/10*0.1)
+dat_1 <- dat_1[, grid_id_no2 := link_no2$nn.idx]
+## merge
+no2 <- data.table(matrix(vector(),0,6))
+names(no2) <- c("lon", "lat", "id", "bdob", "no2", "date")
+no2$bdob <- as.Date(no2$bdob)
+no2$date <- as.Date(no2$date)
+for (i in 1:length(no2_files)) { # 
   no2_temp <- readRDS(paste0(dir_no2,no2_files[i]))
-  no2_temp <- no2_temp[1,dat$grid_id_no2]
+  no2_temp <- no2_temp[1, dat_1$grid_id_no2]
   
-  no2_temp <- data.frame(cbind(dat[,c("LONG","LAT","id")],no2_temp))
-  no2_temp$date <- as.Date(i+as.Date('2006-12-01')-1)
-  names(no2_temp) <- c("lon","lat",'id','no2','date')
-  no2 <- rbind(no2,no2_temp)
+  no2_temp <- data.table(cbind(dat_1[, .(long, lat, id, bdob)], no2_temp))
+  no2_temp$date <- as.Date(i + as.Date('2000-02-01') - 1)
+  names(no2_temp) <- c("lon","lat", "id", "bdob", "no2", "date")
+  no2_temp <- no2_temp[(date >= bdob-280)&(date < bdob)]
+  no2 <- rbind(no2, no2_temp)
   
   rm(no2_temp)
   gc()
   
   if (i%%10 == 0) cat(paste0('Processed ', i, ' of ', length(no2_files),'\n'))
 }
+## save long format
+no2_dat_1 <- no2
+setDT(no2_dat_1)
+fwrite(no2_dat_1, paste0(dir_output, "no2_dat_1_long.csv"))
+# ## transform from long format to wide format
+# no2_dat_1[, pre_day:=bdob-date][]
+# no2_dat_1_wide <- dcast(no2_dat_1, id ~ pre_day, value.var = "no2") # may change no2 var name
+# merge(no2_dat_1_wide, dat[,.(uniqueid_yr,id)], by = "id")
+# ## save wide format part 1
+# fwrite()
+
+####################################
 
 ### lagging and leading
 no2 <- as.data.frame(no2)
 no2 <- no2[order(no2$id,no2$date),]
 no2 <- as.data.table(no2)
 
-no2 = no2[, no2_lag1 := shift(no2,type='lag'),.(id)]
-no2 = no2[, no2_lag2 := shift(no2_lag1,type='lag'),.(id)]
-no2 = no2[, no2_lag3 := shift(no2_lag2,type='lag'),.(id)]
-no2 = no2[, no2_lag4 := shift(no2_lag3,type='lag'),.(id)]
-no2 = no2[, no2_lag5 := shift(no2_lag4,type='lag'),.(id)]
-no2 = no2[, no2_lag6 := shift(no2_lag5,type='lag'),.(id)]
-no2 = no2[, no2_lag7 := shift(no2_lag6,type='lag'),.(id)]
-no2 = no2[, no2_lag8 := shift(no2_lag7,type='lag'),.(id)]
-no2 = no2[, no2_lag9 := shift(no2_lag8,type='lag'),.(id)]
-no2 = no2[, no2_lag10 := shift(no2_lag9,type='lag'),.(id)]
-no2 = no2[, no2_lag11 := shift(no2_lag10,type='lag'),.(id)]
-no2 = no2[, no2_lag12 := shift(no2_lag11,type='lag'),.(id)]
-no2 = no2[, no2_lag13 := shift(no2_lag12,type='lag'),.(id)]
-no2 = no2[, no2_lag14 := shift(no2_lag13,type='lag'),.(id)]
-no2 = no2[, no2_lag15 := shift(no2_lag14,type='lag'),.(id)]
-no2 = no2[, no2_lag16 := shift(no2_lag15,type='lag'),.(id)]
-no2 = no2[, no2_lag17 := shift(no2_lag16,type='lag'),.(id)]
-no2 = no2[, no2_lag18 := shift(no2_lag17,type='lag'),.(id)]
-no2 = no2[, no2_lag19 := shift(no2_lag18,type='lag'),.(id)]
-no2 = no2[, no2_lag20 := shift(no2_lag19,type='lag'),.(id)]
-
-no2$no2_lag713 <- (no2$no2_lag7+no2$no2_lag8+no2$no2_lag9+no2$no2_lag10+no2$no2_lag11+
-                     no2$no2_lag12+no2$no2_lag13)/7
-no2$no2_lag1420 <- (no2$no2_lag14+no2$no2_lag15+no2$no2_lag16+no2$no2_lag17+no2$no2_lag18+
-                      no2$no2_lag19+no2$no2_lag20)/7
+# no2 = no2[, no2_lag1 := shift(no2,type='lag'),.(id)]
+# no2 = no2[, no2_lag2 := shift(no2_lag1,type='lag'),.(id)]
+# no2 = no2[, no2_lag3 := shift(no2_lag2,type='lag'),.(id)]
+# no2 = no2[, no2_lag4 := shift(no2_lag3,type='lag'),.(id)]
+# no2 = no2[, no2_lag5 := shift(no2_lag4,type='lag'),.(id)]
+# no2 = no2[, no2_lag6 := shift(no2_lag5,type='lag'),.(id)]
+# no2 = no2[, no2_lag7 := shift(no2_lag6,type='lag'),.(id)]
+# no2 = no2[, no2_lag8 := shift(no2_lag7,type='lag'),.(id)]
+# no2 = no2[, no2_lag9 := shift(no2_lag8,type='lag'),.(id)]
+# no2 = no2[, no2_lag10 := shift(no2_lag9,type='lag'),.(id)]
+# no2 = no2[, no2_lag11 := shift(no2_lag10,type='lag'),.(id)]
+# no2 = no2[, no2_lag12 := shift(no2_lag11,type='lag'),.(id)]
+# no2 = no2[, no2_lag13 := shift(no2_lag12,type='lag'),.(id)]
+# no2 = no2[, no2_lag14 := shift(no2_lag13,type='lag'),.(id)]
+# no2 = no2[, no2_lag15 := shift(no2_lag14,type='lag'),.(id)]
+# no2 = no2[, no2_lag16 := shift(no2_lag15,type='lag'),.(id)]
+# no2 = no2[, no2_lag17 := shift(no2_lag16,type='lag'),.(id)]
+# no2 = no2[, no2_lag18 := shift(no2_lag17,type='lag'),.(id)]
+# no2 = no2[, no2_lag19 := shift(no2_lag18,type='lag'),.(id)]
+# no2 = no2[, no2_lag20 := shift(no2_lag19,type='lag'),.(id)]
+# 
+# no2$no2_lag713 <- (no2$no2_lag7+no2$no2_lag8+no2$no2_lag9+no2$no2_lag10+no2$no2_lag11+
+#                      no2$no2_lag12+no2$no2_lag13)/7
+# no2$no2_lag1420 <- (no2$no2_lag14+no2$no2_lag15+no2$no2_lag16+no2$no2_lag17+no2$no2_lag18+
+#                       no2$no2_lag19+no2$no2_lag20)/7
 
 no2 = no2[, no2_lead1 := shift(no2,type='lead'),.(id)]
 no2 = no2[, no2_lead2 := shift(no2_lead1,type='lead'),.(id)]
