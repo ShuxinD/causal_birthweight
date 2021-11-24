ce46e79416a9fe427e224e377e4c76dc8b95e81e
ShuxinD <shuxind11@gmail.com>
2021-11-23 21:35:33 -0500

    organize all files and add whole-pregnancy results

---
+++ codes/02causal_analysis/whole_pregnancy/single_pollutant/2truncateIPW_balance.R
@@ -0,0 +1,200 @@
+#' Project: causal_birthweight
+#' Code: estimate stabilized ipw with grid search results and check balance
+#' Input: "MAbirth_for_analyses.csv" birth data
+#' Input: grid search results
+#' Output: weights dataset "MAbirth_all_ipw.csv" with ID and weights
+#' Author: Shuxin Dong
+#' First Ceate Date: 2021-01-21
+
+## setup ----
+rm(list = ls())
+gc()
+
+library(data.table)
+library(polycor)
+library(h2o)
+library(parallel)
+library(wCorr)
+library(ggplot2)
+n_cores <- detectCores() - 1 
+
+setwd("/media/gate/Shuxin/")
+dir_data <- "/media/qnap3/Shuxin/airPollution_MAbirth/"
+dir_gridsearch <- "/media/qnap3/Shuxin/airPollution_MAbirth/causal_birthweight/results/1GridSearchResults/whole_pregnancy/single_pollutant/"
+dir_ipwraw <- "/media/qnap3/Shuxin/airPollution_MAbirth/data/ipw_whole_pregnancy/single_pollutant/"
+
+truncate_ipw <- function(ipw_raw, upper_bound_percentile, lower_bound_percentile){
+  #' ipw_raw: raw stabilized ipw
+  #' upper_bound_percentile: truncation upper bound limit usually 0.99
+  up_bound <- quantile(ipw_raw, upper_bound_percentile)
+  low_bound <- quantile(ipw_raw, lower_bound_percentile)
+  condition <- rep(0, length(ipw_raw))
+  condition[ipw_raw>up_bound] <- 1
+  condition[ipw_raw<low_bound] <- -1
+  ipw <- ifelse(condition==1, up_bound, ifelse(condition==-1, low_bound, ipw_raw))
+  return(ipw) # output truncated ipw
+}
+
+generate_ipw <- function(model_fitted_value, birth_raw_data, exposure_of_interest){
+  numerator <- dnorm((birth_raw_data[,get(exposure_of_interest)]-mean(birth_raw_data[,get(exposure_of_interest)]))/sd(birth_raw_data[,get(exposure_of_interest)]),0,1)
+  gps <- dnorm((birth_raw_data[,get(exposure_of_interest)]-model_fitted_value)/sd(birth_raw_data[,get(exposure_of_interest)]-model_fitted_value),0,1)
+  ipw <- numerator/gps
+  return(ipw)
+}
+
+## load birth data ----
+birth <- fread(paste0(dir_data, "MAbirth_for_analyses.csv"))
+names(birth)
+birth$year <- as.factor(birth$year)
+
+ps_exposures <- c("bc_all", "no2_all")
+ps_vars <- c("year","sex","married","mage", # "cigdpp","cigddp",
+             "clinega","pncgov", 
+             "rf_db_gest","rf_db_other", "rf_hbp_chronic", "rf_hbp_pregn","rf_cervix","rf_prev_4kg", "rf_prev_sga", 
+             "smoker_ddp", # "smoker_dpp",
+             "mrace_1", "mrace_2", "mrace_3", # "mrace_4",
+             "m_edu_1", "m_edu_2", "m_edu_3","m_edu_4", # "m_edu_5",
+             "kotck_1","kotck_2","kotck_3","kotck_4",
+             "m_wg_cat_1","m_wg_cat_2","m_wg_cat_3","m_wg_cat_4",# "m_wg_cat_5",
+             # "log_mhvalue", "log_mhincome",
+             "percentPoverty", "firstborn")
+summary(birth[,..ps_vars])
+
+## load grid search results ----
+gs_bc_all <- fread(paste0(dir_gridsearch, "GridSearch_bc_all_single.csv"))
+gs_no2_all <- fread(paste0(dir_gridsearch, "GridSearch_no2_all_single.csv"))
+
+gs <- rbind(gs_bc_all[objective==min(objective),][,.(objective, rate, depth, ntree)][,exposure:="bc_all"],
+            gs_no2_all[objective==min(objective),][,.(objective, rate, depth, ntree)][,exposure:="no2_all"])
+print(gs)
+
+## model GPS generate IPW ----
+#' set default parameters for H2O
+min.rows <- 10 
+learn.rate <- 0.01
+
+h2o.init(nthreads = n_cores, min_mem_size = "250G", port = 54345)
+for (ps_exposures_i in ps_exposures) {
+  cat("FITTING ", ps_exposures_i, "\n")
+  response <- ps_exposures_i
+  predictor <- c(ps_vars)
+  birth_h2o <- h2o.importFile(paste0(dir_data, "MAbirth_for_analyses.csv"))
+  birth_h2o$year <- as.factor(birth_h2o$year)
+  gbm <- h2o.gbm(y = response, # exposure of interest
+                 x = predictor,
+                 training_frame = birth_h2o,
+                 ntrees = gs[exposure==response, ntree], 
+                 max_depth = gs[exposure==response, depth] ,
+                 min_rows = min.rows,
+                 learn_rate = learn.rate, 
+                 col_sample_rate = gs[exposure==response, rate],
+                 distribution = "gaussian")
+  pred.gbm <- h2o.predict(object = gbm, newdata = birth_h2o)
+  ipw_raw <- generate_ipw(model_fitted_value = as.vector(pred.gbm), birth_raw_data = birth, exposure_of_interest = response)
+  summary(ipw_raw)
+  ipw_id <- data.table(uniqueid_yr = birth[,uniqueid_yr], ipw_raw = ipw_raw)
+  fwrite(ipw_id, file = paste0(dir_ipwraw, ps_exposures_i, "_raw_gbm.csv"))
+}
+h2o.shutdown(prompt = FALSE)
+
+## simple linear regression for GPS ----
+for (ps_exposures_i in ps_exposures) {
+  cat('fitting ', ps_exposures_i, '\n')
+  glm_exposure <- glm(get(ps_exposures_i) ~ # get(ps_exposures[ps_exposures!=ps_exposures_i]) + 
+                        year + sex + married + mage + 
+                        clinega + pncgov +
+                        rf_db_gest + rf_db_other + rf_hbp_chronic + rf_hbp_pregn + rf_cervix + rf_prev_4kg + rf_prev_sga + 
+                        smoker_ddp + 
+                        mrace_1 + mrace_2 + mrace_3 + mrace_4 +
+                        m_edu_1 + m_edu_2 + m_edu_3 + 
+                        kotck_1 + kotck_2 + kotck_3 + kotck_4 + 
+                        m_wg_cat_1 + m_wg_cat_2 + m_wg_cat_3 + m_wg_cat_4 + 
+                        percentPoverty + firstborn,
+                      data = birth)
+  pred.glm <- glm_exposure$fitted
+  ipw_raw <- generate_ipw(model_fitted_value = as.vector(pred.glm), birth_raw_data = birth, exposure_of_interest = ps_exposures_i)
+  summary(ipw_raw)
+  ipw_id <- data.table(uniqueid_yr = birth[,uniqueid_yr], ipw_raw = ipw_raw)
+  fwrite(ipw_id, file = paste0(dir_ipwraw, ps_exposures_i, "_raw_glm.csv"))
+}
+
+## truncate IPW based on balance results ----
+#' change for each exposure
+exposure_interest <- "no2_all"
+upper_percentile <- 0.975
+lower_percentile <- 0.025
+
+response <- exposure_interest
+predictor <- c(ps_vars)
+
+ipw_id_gbm <- fread(paste0(dir_ipwraw, exposure_interest, "_raw_gbm.csv"), colClasses = c("ipw_raw"="numeric"))
+summary(as.numeric(ipw_id_gbm[,ipw_raw]))
+ipw_gbm <- truncate_ipw(as.numeric(ipw_id_gbm[,ipw_raw]), upper_percentile, lower_percentile)
+summary(ipw_gbm)
+par(mfrow=c(2,1))
+hist(as.numeric(ipw_id_gbm[,ipw_raw]))
+plot(density(as.numeric(ipw_id_gbm[,ipw_raw])), lwd = 2, col = "red")
+hist(ipw_gbm)
+plot(density(ipw_gbm), lwd = 2, col = "red")
+
+ipw_id_glm <- fread(paste0(dir_ipwraw, exposure_interest, "_raw_glm.csv"), colClasses = c("ipw_raw"="numeric"))
+summary(as.numeric(ipw_id_glm[,ipw_raw]))
+ipw_glm <- truncate_ipw(as.numeric(ipw_id_glm[,ipw_raw]), upper_percentile, lower_percentile)
+summary(ipw_glm)
+par(mfrow=c(2,1))
+hist(as.numeric(ipw_id_glm[,ipw_raw]))
+plot(density(as.numeric(ipw_id_glm[,ipw_raw])), lwd = 2, col = "red")
+hist(ipw_glm)
+plot(density(ipw_glm), lwd = 2, col = "red")
+
+ac <- matrix(NA,length(predictor),3)
+for (j in 1:length(predictor)){ 
+  ac[j,1] <-  ifelse(!is.factor(birth[,..predictor][[j]]), 
+                     cor(birth[,get(response)], birth[,..predictor][[j]], method = "pearson"),
+                     polyserial(birth[,get(response)], birth[,..predictor][[j]]))
+  ac[j,2] <-  ifelse(!is.factor(birth[,..predictor][[j]]), 
+                     weightedCorr(birth[,get(response)], birth[,..predictor][[j]], method = "pearson", weights = ipw_gbm),
+                     weightedCorr(birth[,get(response)], birth[,..predictor][[j]], method = "polyserial", weights = ipw_gbm))
+  ac[j,3] <-  ifelse(!is.factor(birth[,..predictor][[j]]), 
+                     weightedCorr(birth[,get(response)], birth[,..predictor][[j]], method = "pearson", weights = ipw_glm),
+                     weightedCorr(birth[,get(response)], birth[,..predictor][[j]], method = "polyserial", weights = ipw_glm))
+  cat("finish column", j,"/", length(predictor), "\n")
+}
+
+balance <- data.frame(name.x=predictor,
+                      corr=c(ac[,1],ac[,2],ac[,3]),
+                      weighted = c(rep("unweighted", length(predictor)),rep("weighted_gbm", length(predictor)), rep("weighted_glm", length(predictor))))
+
+balance_plot <- ggplot(balance, aes(x = name.x, y = corr, group = weighted)) +
+  # geom_line(aes(colour = weighted), size = 0.5, linetype = "dashed")+
+  geom_point(aes(colour = weighted), size = 2) +
+  geom_line(aes(colour = weighted), size = 0.5) +
+  geom_hline(yintercept=0, size=0.2) +
+  geom_hline(yintercept=-0.1, size=0.2, linetype = "dashed") +
+  geom_hline(yintercept=0.1, size=0.2, linetype = "dashed") +
+  # geom_hline(yintercept=-0.2, size=0.1, linetype = "dashed") +
+  # geom_hline(yintercept=0.2, size=0.1, linetype = "dashed") +
+  theme(plot.title = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  coord_flip() +
+  theme(legend.position = "bottom", legend.title = element_blank(),
+        text = element_text(size=16))
+balance_plot
+
+dir_plot <- "/media/qnap3/Shuxin/airPollution_MAbirth/causal_birthweight/results/3balancePlots/whole_pregnancy/single_pollutant/two_method/"
+pdf(file = paste0(dir_plot, exposure_interest, ".pdf"))
+balance_plot
+dev.off()
+
+## save the ipw
+assign(paste0("ipw_gbm_", exposure_interest), ipw_gbm)
+assign(paste0("ipw_glm_", exposure_interest), ipw_glm)
+
+## export ipws ----
+IPWs <- data.table(uniqueid_yr = birth[,uniqueid_yr],
+                   ipw_gbm_bc_all,
+                   ipw_glm_bc_all,
+                   ipw_gbm_no2_all,
+                   ipw_glm_no2_all)
+fwrite(IPWs, file = "/media/qnap3/Shuxin/airPollution_MAbirth/data/ipw_whole_pregnancy/single_pollutant/IPWs_all_truncated.csv")
---
+++ codes/02causal_analysis/whole_pregnancy/two_pollutant/3outcomemodel_exportbalance.R
@@ -0,0 +1,225 @@
+#' Project: causal_birthweight
+#' Code: outcome model and check balance again
+#' Input: "MAbirth_for_analyses.csv" "IPWs_all_1104.csv"
+#' Output: ...
+#' Author: Shuxin Dong
+#' First create date: 2021-01-27
+
+## setup ----
+rm(list = ls())
+gc()
+
+library(data.table)
+library(mgcv)
+library(sandwich)
+
+setwd("/media/gate/Shuxin/")
+dir_birth <- "/media/qnap3/Shuxin/airPollution_MAbirth/"
+dir_ipw <- "/media/qnap3/Shuxin/airPollution_MAbirth/data/ipw_whole_pregnancy/two_pollutant/"
+
+## load birth data ----
+birth <- fread(paste0(dir_birth, "MAbirth_for_analyses.csv"))
+# names(birth)
+birth$year <- as.factor(birth$year)
+
+## load IPWs ----
+IPWs <- fread(paste0(dir_ipw, "IPWs_all_truncated.csv"))
+
+## merge ----
+dt <- merge(birth, IPWs, by="uniqueid_yr")
+names(dt)
+
+## IQR ----
+IQRs <- data.table(bc_all=IQR(dt[,bc_all]),
+                   no2_all=IQR(dt[,no2_all]))
+
+## bwg ~ spline ----
+dir_splines <- "/media/qnap3/Shuxin/airPollution_MAbirth/causal_birthweight/results/2mainEffects/whole_pregnancy/two_pollutant/"
+exposures <- c("bc_all", "no2_all")
+
+pdf(file = paste0(dir_splines,"splines.pdf"))
+par(mfrow = c(2,1))
+for (exposures_i in exposures){
+  splines.c <- gam(bwg ~ s(get(exposures_i), bs = "cr", fx=F), family = gaussian, 
+                   data = dt, weights = get(paste0("ipw_gbm_", exposures_i)))
+  plot(splines.c, rug = T)
+  cat("plot ", exposures_i, "\n")
+}
+dev.off()
+
+## bwg ~ exposure ----
+dir_bwg <- "/media/qnap3/Shuxin/airPollution_MAbirth/causal_birthweight/results/2mainEffects/whole_pregnancy/two_pollutant/"
+exposures <- c("bc_all", "no2_all")
+
+results_bwg <- NULL
+for (exposures_i in exposures){
+  results_i <- NULL
+  cat("model ", exposures_i, "\n")
+  mod <- glm(bwg ~ get(exposures_i), 
+                 family = gaussian(link = "identity"), data = dt, 
+                 weights = get(paste0("ipw_gbm_", exposures_i)))
+  results_i <- c(coef(mod)[2],
+                 sqrt(vcovHC(mod)[2,2]),
+                 coef(mod)[2]*IQRs[,get(exposures_i)],
+                 (coef(mod)[2]-qnorm(0.975)*sqrt(vcovHC(mod)[2,2]))*IQRs[,get(exposures_i)],
+                 (coef(mod)[2]+qnorm(0.975)*sqrt(vcovHC(mod)[2,2]))*IQRs[,get(exposures_i)])
+  results_bwg <- rbind(results_bwg, results_i)
+}
+rownames(results_bwg) <- exposures
+colnames(results_bwg) <- c("coef", "robust.se", "diff", "diff_lci", "diff_uci")
+print(results_bwg)
+write.csv(results_bwg, file = paste0(dir_bwg, "bwg_gbm.csv"))
+
+results_bwg <- NULL
+for (exposures_i in exposures){
+  results_i <- NULL
+  cat("model ", exposures_i, "\n")
+  mod <- glm(bwg ~ get(exposures_i), 
+             family = gaussian(link = "identity"), data = dt, 
+             weights = get(paste0("ipw_glm_", exposures_i)))
+  results_i <- c(coef(mod)[2],
+                 sqrt(vcovHC(mod)[2,2]),
+                 coef(mod)[2]*IQRs[,get(exposures_i)],
+                 (coef(mod)[2]-qnorm(0.975)*sqrt(vcovHC(mod)[2,2]))*IQRs[,get(exposures_i)],
+                 (coef(mod)[2]+qnorm(0.975)*sqrt(vcovHC(mod)[2,2]))*IQRs[,get(exposures_i)])
+  results_bwg <- rbind(results_bwg, results_i)
+}
+rownames(results_bwg) <- exposures
+colnames(results_bwg) <- c("coef", "robust.se", "diff", "diff_lci", "diff_uci")
+print(results_bwg)
+write.csv(results_bwg, file = paste0(dir_bwg, "bwg_glm.csv"))
+
+## lbw ~ exposure ----
+dir_lbw <- "/media/qnap3/Shuxin/airPollution_MAbirth/causal_birthweight/results/2mainEffects/whole_pregnancy/two_pollutant/"
+exposures <- c("bc_all", "no2_all")
+
+results_lbw <- NULL
+for (exposures_i in exposures){
+  results_i <- NULL
+  cat("model ", exposures_i, "\n")
+  mod <- glm(lbw ~ get(exposures_i), family = quasibinomial(link = "logit"), data = dt, 
+             weights = get(paste0("ipw_gbm_", exposures_i)))
+  results_i <- c(coef(mod)[2],
+                 sqrt(vcovHC(mod)[2,2]),
+                 exp(coef(mod)[2]*IQRs[,get(exposures_i)]),
+                 exp((coef(mod)[2]-qnorm(0.975)*sqrt(vcovHC(mod)[2,2]))*IQRs[,get(exposures_i)]),
+                 exp((coef(mod)[2]+qnorm(0.975)*sqrt(vcovHC(mod)[2,2]))*IQRs[,get(exposures_i)]))
+  results_lbw <- rbind(results_lbw, results_i)
+}
+rownames(results_lbw) <- exposures
+colnames(results_lbw) <- c("coef", "robust.se", "OR", "OR_lci", "OR_uci")
+print(results_lbw)
+write.csv(results_lbw, file = paste0(dir_lbw, "lbw_gbm.csv"))
+
+results_lbw <- NULL
+for (exposures_i in exposures){
+  results_i <- NULL
+  cat("model ", exposures_i, "\n")
+  mod <- glm(lbw ~ get(exposures_i), family = quasibinomial(link = "logit"), data = dt, 
+             weights = get(paste0("ipw_glm_", exposures_i)))
+  results_i <- c(coef(mod)[2],
+                 sqrt(vcovHC(mod)[2,2]),
+                 exp(coef(mod)[2]*IQRs[,get(exposures_i)]),
+                 exp((coef(mod)[2]-qnorm(0.975)*sqrt(vcovHC(mod)[2,2]))*IQRs[,get(exposures_i)]),
+                 exp((coef(mod)[2]+qnorm(0.975)*sqrt(vcovHC(mod)[2,2]))*IQRs[,get(exposures_i)]))
+  results_lbw <- rbind(results_lbw, results_i)
+}
+rownames(results_lbw) <- exposures
+colnames(results_lbw) <- c("coef", "robust.se", "OR", "OR_lci", "OR_uci")
+print(results_lbw)
+write.csv(results_lbw, file = paste0(dir_lbw, "lbw_glm.csv"))
+
+## export balance ----
+library(ggplot2)
+library(stringr)
+dir_balance <- "/media/qnap3/Shuxin/airPollution_MAbirth/causal_birthweight/results/3balancePlots/whole_pregnancy/two_pollutant/"
+ps_exposures <- c("bc_all","no2_all")
+ps_vars <- c("year","sex","married","mage", # "cigdpp","cigddp",
+             "clinega","pncgov",
+             "rf_db_gest","rf_db_other", "rf_hbp_chronic", "rf_hbp_pregn","rf_cervix","rf_prev_4kg", "rf_prev_sga",
+             "smoker_ddp", # "smoker_dpp",
+             "mrace_1", "mrace_2", "mrace_3", # "mrace_4",
+             "m_edu_1", "m_edu_2", "m_edu_3","m_edu_4", # "m_edu_5",
+             "kotck_1","kotck_2","kotck_3","kotck_4",
+             "m_wg_cat_1","m_wg_cat_2","m_wg_cat_3","m_wg_cat_4",# "m_wg_cat_5",
+             # "log_mhvalue", "log_mhincome",
+             "percentPoverty", "firstborn")
+
+response <- "bc_all" # change everytime
+predictor <- c(ps_vars, ps_exposures[ps_exposures!=response])
+
+ac <- matrix(NA,length(predictor),3)
+for (j in 1:length(predictor)){ 
+  ac[j,1] <-  ifelse(!is.factor(birth[,..predictor][[j]]), 
+                     cor(birth[,get(response)], birth[,..predictor][[j]], method = "pearson"),
+                     polyserial(birth[,get(response)], birth[,..predictor][[j]]))
+  ac[j,2] <-  ifelse(!is.factor(birth[,..predictor][[j]]), 
+                     weightedCorr(birth[,get(response)], birth[,..predictor][[j]], method = "pearson", weights = dt[,get(paste0("ipw_gbm_",response))]),
+                     weightedCorr(birth[,get(response)], birth[,..predictor][[j]], method = "polyserial", weights = dt[,get(paste0("ipw_gbm_",response))]))
+  ac[j,3] <-  ifelse(!is.factor(birth[,..predictor][[j]]), 
+                     weightedCorr(birth[,get(response)], birth[,..predictor][[j]], method = "pearson", weights = dt[,get(paste0("ipw_glm_",response))]),
+                     weightedCorr(birth[,get(response)], birth[,..predictor][[j]], method = "polyserial", weights = dt[,get(paste0("ipw_glm_",response))]))
+  cat("finish column", j,"/", length(predictor), "\n")
+}
+
+balance <- data.table(name.x=predictor,
+                      corr=c(ac[,1],ac[,2],ac[,3]),
+                      weighted = c(rep("unweighted", length(predictor)),rep("weighted_gbm", length(predictor)), rep("weighted_glm", length(predictor))))
+
+balance[,.(AC=mean(abs(corr))), by = weighted]
+wrap_label <- function(x){str_wrap(x, width = 10)}
+balance_plot <- ggplot(balance[weighted!="weighted_glm",], aes(x = name.x, y = corr, group = weighted, colour = weighted)) +
+  # geom_line(aes(colour = weighted), size = 0.5, linetype = "dashed")+
+  geom_point(size = 2) +
+  geom_line(size = 0.5) +
+  geom_hline(yintercept=0, size=0.2) +
+  geom_hline(yintercept=-0.1, size=0.2, linetype = "dashed") +
+  geom_hline(yintercept=0.1, size=0.2, linetype = "dashed") +
+  # geom_hline(yintercept=-0.2, size=0.1, linetype = "dashed") +
+  # geom_hline(yintercept=0.2, size=0.1, linetype = "dashed") +
+  theme(plot.title = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank()) +
+  scale_x_discrete(labels = c("year" = "Birth year (cat.)",
+                              "sex" = "Female baby (b.)",
+                              "married" = "Married mother (b.)",
+                              "mage" = "Mother's age (cont.)",
+                              "clinega" = "Clinical gestational age (cont.)",
+                              "pncgov" = "Goverment support for prenatal care (b.)",
+                              "rf_db_gest" =  "Gestational diabetes (b.)",
+                              "rf_db_other" = "Chronic diabetes (b.)",
+                              "rf_hbp_chronic" = "Chronic hypertension (b.)",
+                              "rf_hbp_pregn" = "Gestational hypertention (b.)",
+                              "rf_cervix" = "Incompetent cervix (b.)",
+                              "rf_prev_4kg" = "With previous >4kg infant (b.)",
+                              "rf_prev_sga" = "With previous SGA infant (b.)",
+                              "smoker_ddp" = "Smoke during pregnancy (b.)",
+                              "mrace_1" = "White mother (b.)",
+                              "mrace_2" = "Black mother (b.)",
+                              "mrace_3" = "Asian/Pacific Islander mother (b.)",
+                              "m_edu_1" = "< high school (b.)",
+                              "m_edu_2" = "high school/G.E.D. (b.)",
+                              "m_edu_3" = "some college (b.)",
+                              "m_edu_4" = "bachelor (b.)",
+                              "kotck_1" = "APNCU index = 1 (b.)",
+                              "kotck_2" = "APNCU index = 2 (b.)",
+                              "kotck_3" = "APNCU index = 3 (b.)",
+                              "kotck_4" = "APNCU index = 4 (b.)",
+                              "m_wg_cat_1" = "Maternal weight change <0 lb. (b.)",
+                              "m_wg_cat_2" = "Maternal weight change [0,15) lb. (b.)",
+                              "m_wg_cat_3" = "Maternal weight change [15-25) lb. (b.)",
+                              "m_wg_cat_4" = "Maternal weight change [25-36) lb. (b.)",
+                              "percentPoverty" = "% Poverty at residence address (cont.)",
+                              "firstborn" = "First-born infant (b.)",
+                              "no2_all" = "Average of daily NO[2] exposure over whole pregnancy (cont.)",
+                              "bc_all" = "Average of daily BC exposure over whole pregnancy (cont.)")) + 
+  scale_colour_discrete(breaks=c("unweighted", "weighted_gbm"),
+                        labels=c("Unweighted correlation", "Weighted correlation")) +
+  coord_flip() + 
+  theme(legend.position = "bottom", legend.title = element_blank(),
+        text = element_text(size=14))
+balance_plot
+
+pdf(file = paste0(dir_balance, response, ".pdf"))
+balance_plot
+dev.off()
